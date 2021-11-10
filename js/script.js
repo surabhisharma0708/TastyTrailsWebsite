7f0acfded1c20695a05e798c117e663e98855877
surabhisharma0708 <surabhi.sharma_cs19@gla.ac.in>
2021-11-09 19:13:11 -0800

    commit

--- js/script.js
+++ js/script.js
@@ -20,10 +20,10 @@ window.onscroll = () =>{
 
 function loader(){
     document.querySelector('.loader-container').classList.add('fade-out');
-
-}
-function fadeOut(){
+  }
+  
+  function fadeOut(){
     setInterval(loader, 3000);
-}
-
-window.onload = fadeOut();
+  }
+  
+  window.onload = fadeOut();
